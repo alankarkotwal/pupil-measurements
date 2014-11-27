d4ab51cfcfcc04c8d1d727fcdb9ff6e3fe05305d
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-11-27 11:06:27 +0530

    update to feed

--- code/pupil.m
+++ code/pupil.m
@@ -7,30 +7,41 @@
 % Make sure you run this file from the pupil-measurements directory
 %**************************************************************************
 
-% Load the image, convert to YCbCr and get luminance
-eye = rgb2ycbcr(imread('images/eye.jpg'));
-eyeL = eye(:, :, 1);
-eyeSize = size(eyeL);
+vid = videoinput('linuxvideo', 1, 'YUYV_640x480');
+src = getselectedsource(vid);
 
-% Do a constrast stretch
-eyeLC = histeq(eyeL);
+vid.FramesPerTrigger = 1;
 
-% Threshold now
-eyeLCT = 1-im2bw(eyeLC, 0.05);
+while 1
 
-% Get connected components and pixel list of largest size
-c = bwconncomp(eyeLCT);
-numPixels = cellfun(@numel, c.PixelIdxList);
-[biggest,idx] = max(numPixels);
-eyeLCT(c.PixelIdxList{idx}) = 2;
-eyeLCT = floor(eyeLCT/2);
+    % Load the image, convert to YCbCr and get luminance
+    preview(vid);
+    start(vid);
+    eye = getdata(vid);
+    eyeL = eye(:, :, 1);
+    eyeSize = size(eyeL);
 
-% Find circles
-s = regionprops(eyeLCT, 'centroid');
-cen = round(s.Centroid);
-ar = bwarea(eyeLCT);
-rad = sqrt(ar/pi);
+    % Do a constrast stretch
+    eyeLC = histeq(eyeL);
 
-% Plot
-imshow(ycbcr2rgb(eye));
-viscircles(cen, rad,'EdgeColor','b');
+    % Threshold now
+    eyeLCT = 1-im2bw(eyeLC, 0.05);
+
+    % Get connected components and pixel list of largest size
+    c = bwconncomp(eyeLCT);
+    numPixels = cellfun(@numel, c.PixelIdxList);
+    [biggest,idx] = max(numPixels);
+    eyeLCT(c.PixelIdxList{idx}) = 2;
+    eyeLCT = floor(eyeLCT/2);
+
+    % Find circles
+    s = regionprops(eyeLCT, 'centroid');
+    cen = round(s.Centroid);
+    ar = bwarea(eyeLCT);
+    rad = sqrt(ar/pi);
+
+    % Plot
+    imshow(ycbcr2rgb(eye));
+    viscircles(cen, rad,'EdgeColor','b');
+    
+end
