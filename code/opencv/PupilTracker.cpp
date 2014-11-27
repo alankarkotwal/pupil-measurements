84b4ece48ad412806a9fcad2cb98ac4929fbfcee
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-11-27 13:10:29 +0530

    added opencv

---
+++ code/opencv/PupilTracker.cpp
@@ -0,0 +1,81 @@
+/************************************************************************
+* Measure and plot pupil parameters from a live feed
+* Main file
+*
+* Author: Alankar Kotwal <alankarkotwal13@gmail.com>
+************************************************************************/
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int thresh = 100;
+int max_thresh = 255;
+int threshVal = 10;
+RNG rng(12345);
+
+int main() {
+
+	//VideoCapture cap(1);
+	Mat frame;
+	frame = imread("../../images/eye.jpg");
+	//char key;
+	namedWindow("Eye Frame", WINDOW_AUTOSIZE);
+	namedWindow("Output", WINDOW_AUTOSIZE);
+	
+	//while(1) {
+		
+		//cap >> frame;
+		imshow("Eye Frame", frame);
+		//key = waitKey(1);
+		//if(key == 'c') {
+		//	break;
+		//}
+		
+		Mat ycbcr;
+		cvtColor(frame, ycbcr, CV_BGR2YCrCb);
+		
+		Mat chan[3];
+		split(ycbcr,chan);
+		Mat y = chan[0];
+		
+		equalizeHist(y, y);
+		threshold(y, y, threshVal, 255, THRESH_BINARY_INV);
+		imshow("Output", y);
+		
+		Mat canny_output;
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+
+		/// Detect edges using canny
+		Canny(y, canny_output, thresh, thresh*2, 3);
+		/// Find contours
+		findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+		
+		int maxArea = 0;
+		int maxIndex = 0;
+		for(int i = 0; i<contours.size(); i++) {
+			int ar = contourArea(contours[i]);
+			std
+			if(ar>maxArea) {
+				maxIndex = i;
+				maxArea = ar;
+			}
+		}
+		
+		Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
+		drawContours(drawing, contours, maxIndex, color, 2, 8, hierarchy, 0, Point());
+
+		// Show in a window
+		namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+		imshow( "Contours", drawing );
+		
+		waitKey(0);
+	//}
+	
+	return 0;
+}
